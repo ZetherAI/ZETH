fc16ddac0780ecc9512bbec233a1b2fbe3031638
Oxlyra <dev@lyraverse.xyz>
2024-12-11 00:36:34 +0100

    init

--- app/(landing)/layout.tsx
+++ app/(landing)/layout.tsx
@@ -16,15 +16,15 @@ export default function RootLayout({
 }>) {
   return (
     <div className="flex h-full bg-fixed overflow-hidden bg-gradient-to-b from-black/30 to-black">
-      <div className="flex h-screen w-full relative">
+      <div className="flex h-full w-full relative overflow-hidden">
         <div className="hide-lg w-full fixed top-0 left-0 z-[10000000000000]">
           <MobileTopbar />
         </div>
         <div className="h-full top-0 left-0 show-lg">
           <Sidebar />
         </div>
-        <div className="w-full relative h-screen overflow-auto pt-[80px] lg:pt-0 ">
-          <div className="w-full container relative h-full">{children}</div>
+        <div className="w-full relative h-full overflow-auto pt-[80px] lg:pt-0 ">
+          <div className="w-full container relative">{children}</div>
         </div>
       </div>
     </div>
