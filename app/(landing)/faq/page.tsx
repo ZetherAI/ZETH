3d437a7240e8ceb420bf5fe42ae0aefc7c319378
Oxlyra <dev@lyraverse.xyz>
2024-12-10 01:17:43 +0100

    init

--- app/(landing)/faq/page.tsx
+++ app/(landing)/faq/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import Image from "next/image";
+// import Image from "next/image";
 
-import { images } from "@/constants";
+// import { images } from "@/constants";
 
 import { DotGrid, Motion } from "@/components";
 
