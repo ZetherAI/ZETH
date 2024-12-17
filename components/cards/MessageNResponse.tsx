02f9415b0ac7eb05d860a888b4b67da12c28fc3f
Oxlyra <dev@lyraverse.xyz>
2024-12-18 00:19:30 +0100

    chat ui

--- components/cards/MessageNResponse.tsx
+++ components/cards/MessageNResponse.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-// import Image from "next/image";
+import Image from "next/image";
 
-// import { images } from "@/constants";
+import { images } from "@/constants";
 
 interface IMessage {
   message: string;
@@ -25,7 +25,20 @@ const MessageNResponse = ({ message, ai_response, time }: IMessage) => {
         /> */}
       </div>
 
-      <p className="ai-message !leading-[1.7em]">{ai_response}</p>
+      <div className="flex gap-2 md:gap-3 w-full justify-start">
+        <Image
+          src={images.lyra}
+          alt="user"
+          className="size-[35px] lg:size-[40px] rounded-full object-cover"
+        />
+        <div className="ai-message flex flex-col items-end gap-1">
+          <p className="w-full">{ai_response}</p>
+          <p className="xs !text-light/50">{time}</p>
+        </div>
+        {/* <div className="size-[35px] rounded-full bg-gradient-to-br from-brand-1/50 to-brand-4/50 backdrop-blur-xl" /> */}
+      </div>
+
+      {/* <p className="ai-message !leading-[1.7em]">{ai_response}</p> */}
     </div>
   );
 };
