8f47e37ee52c63e75ee6aab67d1012e54b468b8b
Oxlyra <dev@lyraverse.xyz>
2024-12-21 11:56:00 +0100

    fixes

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ import { arbitrum, arbitrumSepolia } from "wagmi/chains";
 
 const config = {
 	gameContractAddress: {
-		[arbitrum.id]: "0x5B3b4eC5c0b8fC0A1eE2F3d3f7d6E3C3A0f7a7e6",
+		[arbitrum.id]: "0xE2CDd97dFDE2E274F71A04693F264B01D3fD12bd",
 		[arbitrumSepolia.id]: "0x1Ace536d3575454f169c39e6209308dDA80D8FD8",
 	},
 };
