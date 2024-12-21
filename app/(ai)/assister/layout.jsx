248ea7915c7694ea1644c5b608046092a847fcef
Oxlyra <dev@lyraverse.xyz>
2024-12-21 04:21:54 +0100

    fixes

---
+++ app/(ai)/assister/layout.jsx
@@ -0,0 +1,23 @@
+import { ChatSidebar } from "@/components";
+// import { MotionDiv } from "@/constants/motionProps";
+
+import { ExamplePrompts } from "@/constants/staticText";
+
+export const metadata = {
+	title: "Lyra | The Cosmic Guardian",
+	description:
+		"Lyra is the cosmic guardian, overseeing a growing prize pool and testing the ingenuity and wits of humanity.",
+};
+
+export default function RootLayout({ children }) {
+	return (
+		<div className="flex h-full bg-fixed overflow-hidden bg-gradient-to-b from-black to-dark bg-center">
+			<div className="flex h-full w-full relative overflow-hidden">
+				<div className="h-full top-0 left-0 show-md ">
+					<ChatSidebar examplePrompts={ExamplePrompts} />
+				</div>
+				<div className="w-full relative h-full overflow-auto">{children}</div>
+			</div>
+		</div>
+	);
+}
