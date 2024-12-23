b21222233c4af2f2d06c494701e3a5c02f8b5a2f
Oxlyra <dev@lyraverse.xyz>
2024-12-23 08:02:57 +0100

    fix: refreshed requirements

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
 	/* config options here */
 
-	turbopack: (config) => {
-		config.resolve.fallback = { fs: false, net: false, tls: false };
-		return config;
-	},
-
 	eslint: {
 		ignoreDuringBuilds: true,
 	},
